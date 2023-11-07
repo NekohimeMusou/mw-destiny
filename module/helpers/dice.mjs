5425a00b627cf7eaa415e07e1c7cab746cd6ee45
Nekohime Musou <nekohime.musou@gmail.com>
2023-11-07 13:58:23 -0500

    Progress on roll dialog

--- module/helpers/dice.mjs
+++ module/helpers/dice.mjs
@@ -1,13 +1,7 @@
-/**
- * @argument rollData: Roll Data
- * @argument title: Window title (e.g. "Attribute Roll", "[Skill] Roll")
- */
 export async function rollTest(rollData, title, attr1, skillRank) {
-  const {mod, difficulty, attr2, cancelled} = await showRollDialog(title, attr1);
+  const {mod, difficulty, term2, cancelled} = await showRollDialog(title);
 
   if (cancelled) return;
-
-  const term2 = attr2 ? `@${attr2}` : skillRank;
 
   const rollFormula = `2d6 + @${attr1} + ${term2} + ${mod || 0}`;
 
@@ -16,13 +10,37 @@ export async function rollTest(rollData, title, attr1, skillRank) {
   const total = roll.total;
 }
 
-async function showRollDialog(title) {
+// Return the 2nd term directly: "0" or "@[stat]" or the skill ranks
+async function showRollDialog(title, {attr1=null, skillRank=null}={}) {
   async function _processRollOptions(form) {
-    return {};
+    // If skillRank is 0/empty AND attr2 is empty, it's an unskilled roll
+    // If skillRank > 0, it's a skill roll: return the skill ranks as an int
+    // If attr2 exists, it's an attribute roll; return "@[attr]"
+    const term2 = form.attr2.value ? `@${form.attr2.value}` : `${form.skillRank.value || 0}`;
+    return {
+      mod: parseInt(form.mod.value || 0),
+      difficulty: form.difficulty.value,
+      term2,
+    };
   }
 
   const template = "systems/mw-destiny/templates/dialog/roll-dialog.hbs";
-  const content = await renderTemplate(template);
+  const content = await renderTemplate(template, {title, attr1, skillRank, MWDESTINY: CONFIG.MWDESTINY});
 
-  return {};
+  return new Promise((resolve) => new Dialog({
+    title,
+    content,
+    buttons: {
+      roll: {
+        label: "Roll",
+        callback: (html) => resolve(_processRollOptions(html[0].querySelector("form"))),
+      },
+      cancel: {
+        label: "Cancel",
+        callback: () => resolve({cancelled: true}),
+      },
+    },
+    default: "roll",
+    close: () => resolve({cancelled: true}),
+  }, null).render(true));
 }
