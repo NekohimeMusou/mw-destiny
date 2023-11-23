1d9e4f0ad942c88d33999a5bbf35f486665cf63d
Nekohime Musou <nekohime.musou@gmail.com>
2023-11-23 17:39:54 -0500

    Partial history tab

--- module/config/templates.mjs
+++ module/config/templates.mjs
@@ -2,6 +2,8 @@ export default async function preloadHandlebarsTemplates() {
   const templatePaths = [
     "systems/mw-destiny/templates/actor/parts/header.hbs",
     "systems/mw-destiny/templates/actor/parts/tab-main.hbs",
+    "systems/mw-destiny/templates/actor/parts/tab-history.hbs",
+    // "systems/mw-destiny/templates/actor/parts/tab-personality.hbs",
     "systems/mw-destiny/templates/actor/parts/attributes-pane.hbs",
     "systems/mw-destiny/templates/actor/parts/skills-pane.hbs",
     "systems/mw-destiny/templates/actor/parts/weapons-pane.hbs",
