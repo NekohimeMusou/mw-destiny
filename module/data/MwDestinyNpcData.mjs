5eddab66f93010ea0662c9776e49377dfce68eaa
Nekohime Musou <nekohime.musou@gmail.com>
2023-12-18 17:53:00 -0500

    Add property getters to NPC data (oops)

--- module/data/MwDestinyNpcData.mjs
+++ module/data/MwDestinyNpcData.mjs
@@ -22,4 +22,33 @@ export default class MwDestinyNpcData extends foundry.abstract.DataModel {
       attributes,
     };
   }
+
+  get strBonus() {
+    if (this.attributes.str < 3) return 0;
+    if (this.attributes.str < 5) return 1;
+    return 2;
+  }
+
+  get woundPenalty() {
+    let total = 0;
+
+    for (const [hpPool, attr] of [["physDamage", "str"], ["fatigueDamage", "wil"]]) {
+      const current = this[hpPool].value;
+      const max = this[hpPool].max;
+
+      if (current === max || current > 11 || (current === 11 && this.attributes[attr] < 4)) {
+        continue;
+      } else if (current > 8) {
+        total -= 1;
+      } else if (current > 5) {
+        total -= 2;
+      } else if (current > 2) {
+        total -= 3;
+      } else {
+        total -= 4;
+      }
+    }
+
+    return total;
+  }
 }
