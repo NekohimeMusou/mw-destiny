8c7a0f5e5af36a8047dbe3c94f8c04e0978af794
Nekohime Musou <nekohime.musou@gmail.com>
2023-12-20 20:13:00 -0500

    Increase size of tag array to 6

--- module/data/shared-actor-data.mjs
+++ module/data/shared-actor-data.mjs
@@ -5,7 +5,7 @@ export default function getSharedActorData() {
     description: new fields.HTMLField(),
     tags: new fields.ArrayField(new fields.StringField(), {
       required: true,
-      initial: Array(5).fill(""),
+      initial: Array(6).fill(""),
     }),
     equipment: new fields.ArrayField(new fields.StringField(), {
       initial: Array(8).fill(""),
